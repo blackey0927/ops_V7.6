cc7b5a27dc19eba2ad34de2306472ceb19312c81
blackey0927 <blackey0927@gmail.com>
2026-03-17 08:59:06 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,32 @@
+const CACHE_NAME = 'ops-cache-v1';
+const urlsToCache = [
+  './index.html',
+  './database.js',
+  './manifest.json',
+  'https://cdnjs.cloudflare.com/ajax/libs/react/18.2.0/umd/react.production.min.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/react-dom/18.2.0/umd/react-dom.production.min.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/babel-standalone/7.23.3/babel.min.js'
+];
+
+// 安裝時快取檔案
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// 攔截網路請求，優先從快取讀取（實現秒開）
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response; 
+        }
+        return fetch(event.request);
+      })
+  );
+});
